022be1faf7f05d4a67c2fd3a5e3d66e2e844635d
Danila Balyakin <zx19-95@mail.ru>
2019-12-04 14:31:15 +0300

    reformat code

--- src/com/pacman29/testsber/model/Summary.java
+++ src/com/pacman29/testsber/model/Summary.java
@@ -1,6 +1,8 @@
 package com.pacman29.testsber.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Data
